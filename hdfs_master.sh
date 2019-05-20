2c18c4dae57ffff7a1a8024a3c741608f4f7fa53
Claudio <c.novella.rausell@gmail.com>
2019-05-20 13:12:18 +0200

    until hadoop-env

--- hdfs_master.sh
+++ hdfs_master.sh
@@ -8,5 +8,6 @@ wget http://apache.mirrors.spacedump.net/hadoop/common/hadoop-3.1.2/hadoop-3.1.2
 tar -xzf hadoop-3.1.2.tar.gz
 mv hadoop-3.1.2 hadoop
 rm hadoop-3.1.2.tar.gz
-echo 'PATH=/home/ubuntu/hadoop/bin:/home/ubuntu/hadoop/sbin:$PATH' | tee -a /home/hadoop/.profile
-echo "/usr/lib/jvm/java-8-openjdk-amd64/jre/" | sudo tee -a
+echo 'PATH=/home/ubuntu/hadoop/bin:/home/ubuntu/hadoop/sbin:$PATH' | tee -a /home/ubuntu/.profile
+echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/" | sudo tee -a /home/ubuntu/hadoop/etc/hadoop/hadoop-env.sh
+
