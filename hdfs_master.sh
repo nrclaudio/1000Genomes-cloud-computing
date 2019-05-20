64b0021d031a031d1133f3dc293b97fe5b865219
Claudio <c.novella.rausell@gmail.com>
2019-05-20 13:04:37 +0200

    changed wget

--- hdfs_master.sh
+++ hdfs_master.sh
@@ -5,7 +5,7 @@ IP=`curl ifconfig.me`
 HOSTNAME=`hostname`
 cd
 wget http://apache.mirrors.spacedump.net/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
-tar -xzf hadoop-3.1.2-src.tar.gz
+tar -xzf hadoop-3.1.2.tar.gz
 mv hadoop-3.1.2 hadoop
 rm hadoop-3.1.2.tar.gz
 echo '\nPATH=/home/ubuntu/hadoop/bin:/home/ubuntu/hadoop/sbin:$PATH' | tee -a /home/hadoop/.profile
