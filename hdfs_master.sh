cd31331a01744377de56996c4d9d8dbb07ab4efe
Claudio <c.novella.rausell@gmail.com>
2019-05-20 12:45:06 +0200

    v1.0

---
+++ hdfs_master.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo apt-get install -y openjdk-8-jdk
+IP=`curl ifconfig.me`
+HOSTNAME=`hostname`
+cd
+wget http://apache.mirrors.spacedump.net/hadoop/common/hadoop-3.1.2/hadoop-3.1.2-src.tar.gz
+tar -xzf hadoop-3.1.2-src.tar.gz
+mv hadoop-3.1.2-src hadoop
+rm hadoop-3.1.2-src.tar.gz
+echo '\nPATH=/home/ubuntu/hadoop/bin:/home/ubuntu/hadoop/sbin:$PATH' | tee -a /home/hadoop/.profile
