2f9fa564c4127265357eec8d8a080a6d1016e7ec
Slakky <c.novella.rausell@gmail.com>
2019-06-07 13:50:32 +0000

    fixed plot bug

--- kmerscript_1worker_1file.py
+++ kmerscript_1worker_1file.py
@@ -58,7 +58,7 @@ plt.gcf().canvas.draw()
 tl = plt.gca().get_xticklabels()
 maxsize = max([t.get_window_extent().width for t in tl])
 m = 0.2 # inch margin
-s = maxsize/plt.gcf().dpi*N+2*m
+s = maxsize/plt.gcf().dpi*(len(kmers))+2*m
 margin = m/plt.gcf().get_size_inches()[0]
 plt.gcf().subplots_adjust(left=margin, right=1.-margin)
 plt.gcf().set_size_inches(s, plt.gcf().get_size_inches()[1])
